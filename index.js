6f50b1d53b896b33da8ef511c516efdd15015da5
johnnymoon <johnyisa@yahoo.com>
2025-08-02 22:02:33 +0100

    reduced the MongoDB code. Trying to see why that aspect is failing when the service is run on render

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://johnyisa55:o9rYjkOBBYAStQx1@cluster0.wt56q3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const uri = process.env.MONGODB_URI;
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3060;
 
 // Enable CORS
 app.use(cors());
@@ -19,7 +21,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+var strValue="This is a string value";
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -27,44 +29,36 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
-
         // Make the appropriate DB calls
-        await listDatabases(client);
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     //   await listDatabases(client);
+     strValue="Pinged your deployment. You successfully connected to MongoDB!";
+ //   console.log(strValue);
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
-/**
- * Print the names of all available databases
- * @param {MongoClient} client A MongoClient that is connected to a cluster
- */
-var strValue, strValue2,strValue3;
+//run().catch(console.dir);
+run().catch();
 
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-    strValue= databasesList.databases[0].name;
-        strValue2= databasesList.databases[1].name;
-            strValue3= databasesList.databases[2].name;
-
-};
 
 app.get("/", (req, res) => {
+
+    
     res.json({
         email: "aliogochukwu06@gmail.com",
-        current_datetime: new Date().toISOString(),
-        githuh_url: "https://github.com/Ali-Peter/user-api",
-        test1: strValue,
-         test2: strValue2,
-          test3: strValue3
+       current_datetime: new Date().toISOString(),
+       githuh_url: "https://github.com/Ali-Peter/user-api",
+       test1: strValue
+
     })
+
 })
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-      console.log(`Server running on port ${PORT}`)
+
 })
