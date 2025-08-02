de913a6144da18f3c4465413a49a3ddaa494c986
johnnymoon <johnyisa@yahoo.com>
2025-08-02 20:45:46 +0100

    changed the port number to 3030

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 // Enable CORS
 app.use(cors());
